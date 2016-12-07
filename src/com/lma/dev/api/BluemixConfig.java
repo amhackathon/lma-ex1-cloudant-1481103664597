940669e8ff0a8ac63f4ad91fff62cc491ee9818f
sr13 <lukama@gmail.com>
2016-12-07 14:59:55 +0100

    q

--- src/com/lma/dev/api/BluemixConfig.java
+++ src/com/lma/dev/api/BluemixConfig.java
@@ -1,6 +1,5 @@
-package com.remkohde.dev.liberty.api;
+package com.lma.dev.api;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
